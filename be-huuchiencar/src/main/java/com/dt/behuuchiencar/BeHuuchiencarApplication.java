a918a0691ecc00cf0a6fb75b0b5ebf11409f30a0
Trong-Nguyen-DT <103616323+Trong-Nguyen-DT@users.noreply.github.com>
2024-03-16 20:54:18 +0700

    "fist commit"

---
+++ be-huuchiencar/src/main/java/com/dt/behuuchiencar/BeHuuchiencarApplication.java
@@ -0,0 +1,13 @@
+package com.dt.behuuchiencar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeHuuchiencarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeHuuchiencarApplication.class, args);
+	}
+
+}
---
+++ be-huuchiencar/src/test/java/com/dt/behuuchiencar/BeHuuchiencarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dt.behuuchiencar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeHuuchiencarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
