c0cac30fd8abff995e2cb8d1b71b489c0c0b3e33
Trong-Nguyen-DT <103616323+Trong-Nguyen-DT@users.noreply.github.com>
2024-03-19 15:28:57 +0700

    "done getAllCustomer"

--- fe-huuchiencar/src/pages/customer/CustomerPage.jsx
+++ fe-huuchiencar/src/pages/customer/CustomerPage.jsx
@@ -1,10 +1,12 @@
 import NavBar from "../../components/navBar/NavBar";
 import Header from "../../components/header/Header";
+import Customer from "../../components/customer/Customer";
 
 const CustomerPage= () => {
     return ( 
         <>
             <Header/>
+            <Customer/>
         </>
      );
 }
