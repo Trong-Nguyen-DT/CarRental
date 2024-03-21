92e6e0cd6dc00c35d6a6843a236612d9973c1400
suongchan <minhsuong19102002@gmail.com>
2024-03-21 12:52:51 +0700

    switch update

--- fe-huuchiencar/src/components/customer/CreateCustomer.jsx
+++ fe-huuchiencar/src/components/customer/CreateCustomer.jsx
@@ -2,8 +2,10 @@ import React, { useState } from "react";
 import { Button, Form, Modal } from "react-bootstrap";
 import { createCustomer } from "../../services/UserService";
 import { toast } from "react-toastify";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-const CreateCustomer = ({ show, handleClose }) => {
+const CreateCustomer = () => {
     const [formData, setFormData] = useState({
         name: "",
         phoneNumber: "",
@@ -14,6 +16,7 @@ const CreateCustomer = ({ show, handleClose }) => {
         driverLicenseBackImage: null,
     });
 
+    const [show, setShow] = useState(false);
     const [idCardFrontImage, setIdCardFrontImage] = useState(null);
     const [idCardBackImage, setIdCardBackImage] = useState(null);
     const [driverLicenseFrontImage, setDriverLicenseFrontImage] = useState(null);
@@ -41,7 +44,7 @@ const CreateCustomer = ({ show, handleClose }) => {
             reader.readAsDataURL(file);
         }
     };
-    
+
     const handleIdCardBackChange = (event) => {
         const file = event.target.files[0];
         if (file) {
@@ -56,7 +59,7 @@ const CreateCustomer = ({ show, handleClose }) => {
             reader.readAsDataURL(file);
         }
     };
-    
+
     const handleDriverLicenseFrontChange = (event) => {
         const file = event.target.files[0];
         if (file) {
@@ -71,7 +74,7 @@ const CreateCustomer = ({ show, handleClose }) => {
             reader.readAsDataURL(file);
         }
     };
-    
+
     const handleDriverLicenseBackChange = (event) => {
         const file = event.target.files[0];
         if (file) {
@@ -87,7 +90,7 @@ const CreateCustomer = ({ show, handleClose }) => {
         }
     };
 
-    const handleSubmit = async  () => {
+    const handleSubmit = async () => {
         const formDataToSend = new FormData();
         formDataToSend.append("name", formData.name);
         formDataToSend.append("phone", formData.phoneNumber);
@@ -107,71 +110,84 @@ const CreateCustomer = ({ show, handleClose }) => {
         }
     };
 
+    const handleShowCreateModal = () => {
+        setShow(true);
+    };
+
+    const handleClose = () => {
+        setShow(false);
+    }
+
     return (
-        <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-                <Modal.Title>Tạo khách hàng mới</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form>
-                    <Form.Group controlId="name">
-                        <Form.Label>Tên khách hàng</Form.Label>
-                        <Form.Control type="text" name="name" placeholder="Nhập tên khách hàng" onChange={handleInputChange} />
-                    </Form.Group>
+        <>
+            <button onClick={handleShowCreateModal}>
+                    <i className="uil uil-plus"></i>
+            </button>
+            <Modal show={show} onHide={handleClose} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>Tạo khách hàng mới</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group controlId="name">
+                            <Form.Label>Tên khách hàng</Form.Label>
+                            <Form.Control type="text" name="name" placeholder="Nhập tên khách hàng" onChange={handleInputChange} />
+                        </Form.Group>
 
-                    <Form.Group controlId="phoneNumber">
-                        <Form.Label>Số điện thoại</Form.Label>
-                        <Form.Control type="text" name="phoneNumber" placeholder="Nhập số điện thoại" onChange={handleInputChange} />
-                    </Form.Group>
+                        <Form.Group controlId="phoneNumber">
+                            <Form.Label>Số điện thoại</Form.Label>
+                            <Form.Control type="text" name="phoneNumber" placeholder="Nhập số điện thoại" onChange={handleInputChange} />
+                        </Form.Group>
 
-                    <Form.Group controlId="idCard">
-                        <Form.Label>Căn cước công dân</Form.Label>
-                        <Form.Control type="text" name="idCard" placeholder="Nhập số căn cước công dân" onChange={handleInputChange} />
-                    </Form.Group>
+                        <Form.Group controlId="idCard">
+                            <Form.Label>Căn cước công dân</Form.Label>
+                            <Form.Control type="text" name="idCard" placeholder="Nhập số căn cước công dân" onChange={handleInputChange} />
+                        </Form.Group>
 
-                    <Form.Group controlId="idCardFront">
-                        <Form.Label>Ảnh mặt trước căn cước công dân</Form.Label>
-                        <Form.Control type="file" onChange={handleIdCardFrontChange} />
-                        {idCardFrontImage && (
-                            <img src={idCardFrontImage} alt="Ảnh mặt trước căn cước công dân" style={{ marginTop: 10, maxWidth: '100%', height: 'auto' }} />
-                        )}
-                    </Form.Group>
+                        <Form.Group controlId="idCardFront">
+                            <Form.Label>Ảnh mặt trước căn cước công dân</Form.Label>
+                            <Form.Control type="file" onChange={handleIdCardFrontChange} />
+                            {idCardFrontImage && (
+                                <img src={idCardFrontImage} alt="Ảnh mặt trước căn cước công dân" style={{ marginTop: 10, maxWidth: '100%', height: 'auto' }} />
+                            )}
+                        </Form.Group>
 
-                    <Form.Group controlId="idCardBack">
-                        <Form.Label>Ảnh mặt sau căn cước công dân</Form.Label>
-                        <Form.Control type="file" onChange={handleIdCardBackChange} />
-                        {idCardBackImage && (
-                            <img src={idCardBackImage} alt="Ảnh mặt sau căn cước công dân" style={{ marginTop: 10, maxWidth: '100%', height: 'auto' }} />
-                        )}
-                    </Form.Group>
+                        <Form.Group controlId="idCardBack">
+                            <Form.Label>Ảnh mặt sau căn cước công dân</Form.Label>
+                            <Form.Control type="file" onChange={handleIdCardBackChange} />
+                            {idCardBackImage && (
+                                <img src={idCardBackImage} alt="Ảnh mặt sau căn cước công dân" style={{ marginTop: 10, maxWidth: '100%', height: 'auto' }} />
+                            )}
+                        </Form.Group>
 
-                    <Form.Group controlId="driverLicenseFront">
-                        <Form.Label>Ảnh mặt trước bằng lái</Form.Label>
-                        <Form.Control type="file" onChange={handleDriverLicenseFrontChange} />
-                        {driverLicenseFrontImage && (
-                            <img src={driverLicenseFrontImage} alt="Ảnh mặt trước bằng lái" style={{ marginTop: 10, maxWidth: '100%', height: 'auto' }} />
-                        )}
-                    </Form.Group>
+                        <Form.Group controlId="driverLicenseFront">
+                            <Form.Label>Ảnh mặt trước bằng lái</Form.Label>
+                            <Form.Control type="file" onChange={handleDriverLicenseFrontChange} />
+                            {driverLicenseFrontImage && (
+                                <img src={driverLicenseFrontImage} alt="Ảnh mặt trước bằng lái" style={{ marginTop: 10, maxWidth: '100%', height: 'auto' }} />
+                            )}
+                        </Form.Group>
 
-                    <Form.Group controlId="driverLicenseBack">
-                        <Form.Label>Ảnh mặt sau bằng lái</Form.Label>
-                        <Form.Control type="file" onChange={handleDriverLicenseBackChange} />
-                        {driverLicenseBackImage && (
-                            <img src={driverLicenseBackImage} alt="Ảnh mặt sau bằng lái" style={{ marginTop: 10, maxWidth: '100%', height: 'auto' }} />
-                        )}
-                    </Form.Group>
+                        <Form.Group controlId="driverLicenseBack">
+                            <Form.Label>Ảnh mặt sau bằng lái</Form.Label>
+                            <Form.Control type="file" onChange={handleDriverLicenseBackChange} />
+                            {driverLicenseBackImage && (
+                                <img src={driverLicenseBackImage} alt="Ảnh mặt sau bằng lái" style={{ marginTop: 10, maxWidth: '100%', height: 'auto' }} />
+                            )}
+                        </Form.Group>
 
-                </Form>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                    Đóng
-                </Button>
-                <Button variant="primary" onClick={handleSubmit}>
-                    Tạo khách hàng
-                </Button>
-            </Modal.Footer>
-        </Modal>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Đóng
+                    </Button>
+                    <Button variant="primary" onClick={handleSubmit}>
+                        Tạo khách hàng
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
     );
 }
 
